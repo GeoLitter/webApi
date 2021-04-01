c55b2ac41553068732898b49b830306e8258f3df
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-04-02 00:55:31 +0800

    updated files

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,8 @@
-const { MONGO_HOST, MONGO_PORT, MONGO_DATABASE } = process.env;
+const { MONGO_HOST, MONGO_PORT, MONGO_DATABASE, DATA_URI } = process.env;
 
 export const dbConnection = {
-  url: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,
+  url: `${DATA_URI}`,
+  // url: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
