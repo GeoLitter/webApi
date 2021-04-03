3d723779121d6a2d730ce4f3320cf09a4e0b983e
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-04-04 02:11:27 +0800

    updated user schema

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -2,6 +2,10 @@ import { model, Schema, Document } from 'mongoose';
 import { User } from '../interfaces/users.interface';
 
 const userSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true,
@@ -11,6 +15,13 @@ const userSchema: Schema = new Schema({
     type: String,
     required: true,
   },
+  avatar: {
+    type: String
+  }, 
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const userModel = model<User & Document>('User', userSchema);
